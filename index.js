906c2412b4152007e38c4f8bce9d06eaec1a2797
heuristic_machina <machine_learner@yahoo.com>
2025-02-13 20:15:59 -0700

    firebase db initialized

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js"
+const firebaseConfig = {}
+const app = initializeApp(firebaseConfig);
+console.log(app)
+
 let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
